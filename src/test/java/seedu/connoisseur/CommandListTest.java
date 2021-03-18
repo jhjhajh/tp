35fa9c39d57e37392c5555140d1761088a7bf78e
angjiahui <angjiahui@users.noreply.github.com>
2021-03-19 02:49:09 +0800

    Added some JUnit files

---
+++ src/test/java/seedu/connoisseur/CommandListTest.java
@@ -0,0 +1,16 @@
+package seedu.connoisseur;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import seedu.connoisseur.commandlist.CommandList;
+
+import org.junit.jupiter.api.Test;
+
+class CommandListTest {
+
+    @Test
+    public void testPrintHelp() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/seedu/connoisseur/ParserTest.java
@@ -0,0 +1,19 @@
+package seedu.connoisseur;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.connoisseur.parser.Parser;
+import seedu.connoisseur.commandlist.CommandList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class ParserTest {
+
+    @Test
+    public void testDetermineCommand() {
+        assertTrue(true);
+    }
+
+}
