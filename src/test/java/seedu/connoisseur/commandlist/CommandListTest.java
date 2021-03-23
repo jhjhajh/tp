db7556e7a37c43a770ceaeebf31c09095a076123
angjiahui <angjiahui@users.noreply.github.com>
2021-03-24 06:52:57 +0800

    -

--- src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
+++ src/test/java/seedu/connoisseur/commandlist/CommandListTest.java
@@ -1,35 +1,45 @@
 package seedu.connoisseur.commandlist;
 
-import org.junit.Before;
+import seedu.connoisseur.review.Review;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.After;
+import org.junit.jupiter.api.AfterEach;
+import seedu.connoisseur.commandlist.CommandList;
+import seedu.connoisseur.sorter.SortMethod;
+import seedu.connoisseur.sorter.Sorter;
+import seedu.connoisseur.storage.Storage;
+import seedu.connoisseur.ui.Ui;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 
 public class CommandListTest {
 
     private final PrintStream originalOut = System.out;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    public ArrayList<Review> reviewList;
+    private Sorter sorter;
+    private Ui ui;
+    private Storage storage;
 
-    @Before
+    public void CommandListTest (Ui ui, Storage storage) {
+        this.ui = ui;
+        this.storage = storage;
+        reviewList = new ArrayList<Review>();
+        sorter = new Sorter(SortMethod.DATE_LATEST);
+    }
+
+    @BeforeEach
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
 
-    @After
+    @AfterEach
     public void restoreStreams() {
         System.setOut(originalOut);
     }
 
-    @Test
-    public void listReview_noReviewsExist() {
-
-        setUpStreams();
-        System.out.print("You have no reviews, type 'new' to start!");
-        assertEquals("You have no reviews, type 'new' to start!", outContent.toString());
-        restoreStreams();
-    }
 }
